b1fdc3d2863bb0a9bd8aa5cdecdeeaf69810a3aa
NarendraKumar <91892560+NarendraKumar88400@users.noreply.github.com>
2023-01-20 10:19:04 +0530

    Add files via upload

---
+++ SignUp-Form/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const SignUpBtn = document.querySelector(".green-bg button");
+
+SignUpBtn.addEventListener("click",() => {
+    container.classList.toggle("change");
+});
